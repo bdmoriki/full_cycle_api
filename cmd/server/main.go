4f4fe361ebc2fdbfa10cb5c7af35d40594eeeda0
Bruno <dupim1@hotmail.com>
2024-08-17 22:25:58 -0300

    Adicionando aula 3 - finalizando configuração

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "github.com/bdmoriki/full_cycle_api/configs"
 
+func main() {
+	config, err := configs.LoadConfig(".env")
+	if err != nil {
+		panic(err)
+	}
 }
